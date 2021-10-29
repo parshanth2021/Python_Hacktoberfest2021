711960a22497db0ecc3a38153b78ed16cfd06eae
Aman Sharma <72340613+AmanSharma9050@users.noreply.github.com>
2021-10-29 18:12:29 +0530

    Add files via upload

--- class.py
+++ class.py
@@ -1,29 +1,22 @@
-# class person:
-#     name = 'abc'
-#     gender= 'male'
-     
-#     def showinfo(self):
-#         print('Name: ', self.name )
-#         print('gender: ', self.gender)
+class Employee :
+    no =4
+    pass
+    def toprint(self):
+        return ("name is" ,self.name ,"age is", self.age ,"pay is", self.pay)
+harry= Employee()
+larry= Employee()
+parry= Employee()
+parry.name="parry"
+harry.name="harry"
+larry.name="larry"
+parry.age=23
+harry.age=24
+larry.age=25
+parry.pay=234
+harry.pay=235
+larry.pay=236
+print(harry.toprint())
 
-# p1=person()
-# p1.showinfo()
-class person:
-    counter=0
-    def __init__(self, a='Ravi', b= 'male'):
-        self.name= a
-        self.gender= b
-        person.counter= person.counter+1
-    def showinfo(self):
-        print('Name: ', self.name)
-        print('Gender: ', self.gender)
-    @classmethod
-    def showcount(cls):
-        print('No. of persons so far= ', cls.counter)
-p1= person('sophie', 'female')
-p1.showinfo()
-p2= person('stefen', 'male')
-p2.showinfo()
-p3= person()
-p3.showinfo()
-person.showcount()
+
+
+
